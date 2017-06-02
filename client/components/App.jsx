fd4c575704f93f6f1d1c8001f61ba4ddf95ddbf4
AnnaUlyanova <a.ulyanova89@gmail.com>
2017-06-02 12:17:24 +1200

    first commit

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateReactClass from 'create-react-class'
+
+export default CreateReactClass({
+  render() {
+    return (
+      <div>
+      <h1>Hi</h1>
+      </div>
+    )
+  }
+})
